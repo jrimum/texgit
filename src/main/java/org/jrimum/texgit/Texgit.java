1fe0c31304683e4efd50d9efbd01d190bc9f15f8
Gilmar P.S.L <gilmatryx@gmail.com>
2010-11-22 16:57:53 +0000

    Revisão do código

--- src/main/java/org/jrimum/texgit/Texgit.java
+++ src/main/java/org/jrimum/texgit/Texgit.java
@@ -7,34 +7,31 @@ import java.io.File;
 
 import org.jrimum.texgit.engine.TexgitManager;
 
-
-
-
 public final class Texgit {
 
-	public static final FlatFile<Record> createFlatFile(File xmlDef)throws TexgitException{
-		
-		FlatFile<Record> iFlatFile = null;
-		
-		if (isNotNull(xmlDef))
-			iFlatFile = TexgitManager.buildFlatFile(xmlDef);
-		
-		return iFlatFile;
+	public static final FlatFile<Record> createFlatFile(File xmlDef)
+			throws TexgitException {
+
+		if (isNotNull(xmlDef)){
+			
+			return TexgitManager.buildFlatFile(xmlDef);
+		}
+
+		return null;
 	}
-	
-	public static final FlatFile<Record> createFlatFile(String xmlDef)throws TexgitException{
-		
-		FlatFile<Record> iFlatFile = null;
-		
-		try{
-		
-		if(isNotBlank(xmlDef))	
-			iFlatFile = createFlatFile(new File(xmlDef));
-	
-		}catch (Exception e) {
+
+	public static final FlatFile<Record> createFlatFile(String xmlDef)
+			throws TexgitException {
+
+		try {
+
+			if (isNotBlank(xmlDef))
+				return createFlatFile(new File(xmlDef));
+
+		} catch (Exception e) {
 			throw new TexgitException(e);
 		}
 		
-		return iFlatFile;
+		return null;
 	}
 }
